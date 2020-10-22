9e6808e2036e471df6529aa7ca3a276c665e93d3
Tsungyu Yu <ytyubox@gmail.com>
2020-10-22 15:24:35 +0800

    can gen index.html by run.sh

--- Sources/LearnHTMLByPlot/main.swift
+++ Sources/LearnHTMLByPlot/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import Plot
+let html = HTML(
+    .body(
+        .h2("Countries and their capitals"),
+        .table(
+            .tr(.th("Country"), .th("Capital")),
+            .tr(.td("Sweden"), .td("Stockholm")),
+            .tr(.td("Japan"), .td("Tokyo"))
+        ),
+        .h2("List of ", .strong("programming languages")),
+        .ul(
+            .li("Swift"),
+            .li("Objective-C"),
+            .li("C")
+        )
+    )
+)
+print(html.render(indentedBy: .spaces(4)))
